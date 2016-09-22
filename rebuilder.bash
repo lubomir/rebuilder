81ce59d8054c259f3731ec90c3287a27c809abda
Lubomír Sedlář <lsedlar@redhat.com>
2016-09-22 16:25:20 +0200

    Add bash completion

---
+++ rebuilder.bash
@@ -0,0 +1,44 @@
+# rebuilder bash completion
+
+_rebuilder()
+{
+    in_array()
+    {
+        local i
+        for i in $2; do
+            [[ "$i" = "$1" ]] && return 0
+        done
+        return 1
+    }
+
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    local prev=${COMP_WORDS[COMP_CWORD - 1]}
+
+    local commands="mock build scratch"
+    local completions=""
+
+    case "$prev" in
+        "rebuilder")
+            COMPREPLY=( $(compgen -W "$commands --no-rebase" -- "$cur") )
+            return 0
+            ;;
+        "scratch")
+            completions="--srpm"
+            ;;
+    esac
+
+
+    if in_array "$prev" "$commands --srpm"; then
+        completions="$completions $(_rebuilder_branch)"
+    else
+        completions="$completions $commands"
+    fi
+
+    COMPREPLY=( $(compgen -W "$completions" -- "$cur") )
+} &&
+complete -F _rebuilder rebuilder
+
+_rebuilder_branch()
+{
+    git for-each-ref --format "%(refname:short)" "refs/heads"
+}
--- setup.py
+++ setup.py
@@ -2,6 +2,16 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+try:
+    from subprocess import getstatusoutput
+except:
+    from commands import getstatusoutput
+
+
+def bash_completion_dir():
+    (sts, output) = getstatusoutput(
+        'pkg-config --variable=completionsdir bash-completion')
+    return output if not sts and output else '/etc/bash_completion.d'
 
 long_description = ''
 
@@ -26,6 +36,7 @@ setup(
     test_suite='nose.collector',
     packages=find_packages(),
     include_package_data=True,
+    data_files=[(bash_completion_dir(), ['rebuilder.bash'])],
     entry_points="""
     [console_scripts]
     rebuilder = rebuilder:main
